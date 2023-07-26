320b2d6c9257e438d293c0c10354550fb5e455e8
mateusvbfontes <87830682+mateusvbfontes@users.noreply.github.com>
2023-07-26 16:21:54 -0300

    Update bouncingballeffect.h

--- include/effects/strip/bouncingballeffect.h
+++ include/effects/strip/bouncingballeffect.h
@@ -19,6 +19,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with Nightdriver.  It is normally found in copying.txt
 //    If not, see <https://www.gnu.org/licenses/>.
+//    TODO: ball physics in pinball game
 //
 // Description:
 //
